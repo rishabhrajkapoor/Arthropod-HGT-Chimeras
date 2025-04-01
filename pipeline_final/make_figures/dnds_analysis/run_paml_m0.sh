331c6c94a971d602766a266c4b6ee75f318b7731
rishabhrajkapoor <139572879+rishabhrajkapoor@users.noreply.github.com>
2025-04-01 16:53:13 -0400

    Add files via upload

---
+++ pipeline_final/make_figures/dnds_analysis/run_paml_m0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p shared # Partition to submit to (comma separated)
+#SBATCH -J fixed_paml # Job name
+#SBATCH -n 25 # Number of cores
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH --mem 25G
+#SBATCH -t 0-08:00
+#SBATCH --mail-user=rkapoor@g.harvard.edu
+#SBATCH --mail-type=FAIL
+
+cd dnds/$1
+
+singularity exec /cvmfs/singularity.galaxyproject.org/p/a/paml:4.10.6--h031d066_1 codeml /n/holylabs/LABS/extavour_lab/Users/rkapoor/pipeline_final/dnds_scripts/m0_fullgene.ctl
